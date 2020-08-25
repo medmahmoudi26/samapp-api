fd2c75e44cc4e723ef0517da0c5d6fe3bd2996e9
medmahmoudi26 <med.mahmoudi2001@gmail.com>
2020-08-25 03:08:53 +0100

    upload diplome

--- models/diplome.js
+++ models/diplome.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DiplomeSchema = new Schema ({
+  userId  : {type: String, required: true},
   diplome: {type: String, required: true},
   annediplome: {type: String, required: true},
   filediplome: {type: String, required: true}
